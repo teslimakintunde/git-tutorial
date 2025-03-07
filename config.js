ad603abd90f2c82852a81968a71fdbe147a0bfd4
Akintunde Teslim Akinmuyiwa <akinmuyiwateslim@gmail.com>
2025-03-07 17:16:04 +0100

    version 1 restored

--- config.js
+++ config.js
@@ -1,5 +1,2 @@
 console.log("config.js");
-console.log("config.js,2");
-console.log("version 1 to version 2");
-console.log("version 2 added");
-console.log("version 3 added");
+console.log("version 1");
--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 console.log("index.js");
-console.log("index.js 2");
-console.log("version 1 to version 2");
-console.log("version 2 added");
-console.log("version 3 added");
+console.log("version 1");

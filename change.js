edecd4ee48621885f0ac97979f92b6de31bebbe7
Akintunde Teslim Akinmuyiwa <akinmuyiwateslim@gmail.com>
2025-03-07 17:09:58 +0100

    version 3

--- change.js
+++ change.js
@@ -2,3 +2,4 @@ console.log("change.js");
 console.log("change.js 2");
 console.log("version 1 to version 2");
 console.log("version 2 added");
+console.log("version 3 added");
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ console.log("index.js");
 console.log("index.js 2");
 console.log("version 1 to version 2");
 console.log("version 2 added");
+console.log("version 3 added");

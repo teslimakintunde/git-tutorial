bf6be1e03645285b277aca6c3d558a13ed322c18
Akintunde Teslim Akinmuyiwa <akinmuyiwateslim@gmail.com>
2025-03-10 09:56:07 +0100

    adding this changes to version 3

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 console.log("confog.js");
 console.log("versiion 3");
 console.log("updated from version 2 to version 3");
+console.log("updated version 3");
+console.log("i am adding this to updated version 3");
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 console.log("index.js");
 console.log("version 3");
 console.log("updated from version 2 to version 3");
+console.log("updated version 3");
+console.log("i am adding this to updated version 3");
